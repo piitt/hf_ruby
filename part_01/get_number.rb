5068265dcf0eac715e1017b3cf15b8ac95e31718
Alexandr Plakhotnikov <mxplaha@gmail.com>
2019-03-23 14:48:12 +0300

    get number my version v.2

--- part_01/get_number.rb
+++ part_01/get_number.rb
@@ -1,26 +1,26 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
-machine_number = rand(100) + 1
+ii_number = rand(100) + 1
 count_step = 0
-game_status = true
+game_over = false
 
 puts "Добро пожаловать в игру 'Угадай число'"
 print "Представтесь пожалуйста: "
 user_name = gets.chomp
 puts "Приятно познакомиться #{user_name}!"
 puts 'Я загадал число от 1 до 100, отгдайте его'
-while (count_step != 10 && game_status)
+until (count_step == 10 || game_over)
   puts "У вас есть #{10 - count_step} попыток"
   print 'Пожалуйста введите число: '
   user_answer = gets.to_i
   count_step += 1
-  if user_answer == machine_number
+  if user_answer == ii_number
     puts "Поздравляю #{user_name} ВЫ ВЫИГРАЛИ!!!"
     puts "Вы угадали число за #{count_step} попыток"
-    game_status = false
+    game_over = true
   else
-    if user_answer < machine_number
+    if user_answer < ii_number
       puts 'Ваше число МЕНЬШЕ'
     else
       puts 'Ваше число БОЛЬШЕ'
@@ -28,4 +28,6 @@ while (count_step != 10 && game_status)
   end
 end
 
-if game_status  then puts "ВЫ ПРОИГРАЛИ! Я загадал #{machine_number}" end
+unless game_over
+  puts "ВЫ ПРОИГРАЛИ! Я загадал #{ii_number}"
+end
