d4b937eb64b3e780febf880415959adb564f29ed
Alexandr Plakhotnikov <mxplaha@gmail.com>
2019-03-29 14:41:18 +0300

    part 6 finaly rewiev app

--- part_06/review/review.rb
+++ part_06/review/review.rb
@@ -1,10 +1,23 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 #
+def find_adjective(string)
+  words = string.split(' ')
+  index = words.find_index('is')
+  words[index + 1]
+end
+
 lines = []
 File.open('data/reviews.txt', 'r') do |file|
   lines = file.readlines
 end
 
 relevant_lines = lines.find_all {|line| line.include?('Truncated')}
-p relevant_lines
+rewiev = relevant_lines.reject {|line| line.include?('--')}
+
+adjectives = rewiev.map do |string|
+  "'#{find_adjective(string).capitalize}'"
+end
+
+puts "The critics agree, Truncated is:"
+puts adjectives
