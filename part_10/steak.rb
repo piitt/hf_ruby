90638b645a001da76adae2b0743b568eac5031bc
Alexandr Plakhotnikov <mxplaha@gmail.com>
2019-04-03 14:02:13 +0300

    part 10 module Comparable

---
+++ part_10/steak.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+#
+class Steak
+  include Comparable
+  attr_accessor :grade
+  GRADE_SCORES = {'Prime' => 3, 'Choice' => 2, 'Select' => 1}\
+
+  def <=>(other)
+    if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+      return -1
+    elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+      return 0  
+    else
+      return 1
+    end
+  end
+end
+
+prime = Steak.new
+prime.grade = 'Prime'
+
+choise = Steak.new
+choise.grade = 'Choice'
+
+select = Steak.new
+select.grade = 'Select'
+
+puts "prime > choise: #{prime > choise}"
+puts "prime < select: #{prime < select}"
+puts "select == select: #{select == select}"
+puts "select <= select: #{select <= select}"
+puts "select >= choise: #{select >= choise}"
+print "choise.between?(select, prime): "
+puts choise.between?(select, prime)
